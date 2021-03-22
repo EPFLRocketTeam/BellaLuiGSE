ca1f64db575316426a10bf57cd1aa656fe3765a1
Lucas Pallez <lucas.pallez@hotmail.com>
2021-03-22 21:07:33 +0100

    Improved multi-channel ADC readings and added conversion stubs
    Functional but some bugs remain

--- Application/Inc/GSE/sensor.h
+++ Application/Inc/GSE/sensor.h
@@ -14,11 +14,12 @@ void sensors_init(void);
 void TK_telemetry_control(void const * argument);
 void TK_sensors_control(void const * argument);
 
-void read_tank_temp(void);
-void read_anemo(void);
-void read_hose_temp(void);
-void read_hose_pressure(void);
-void read_load_cell(void);
+//void read_tank_temp(void);
+//void read_anemo(void);
+//void read_hose_temp(void);
+//void read_hose_pressure(void);
+//void read_load_cell(void);
 
+float sensor_conversion(uint8_t sensor, float raw_data);
 
 #endif /* APPLICATION_HOSTBOARD_INC_GSE_SENSOR_TELEMETRY_H_ */
--- Application/Src/GSE/sensor.c
+++ Application/Src/GSE/sensor.c
@@ -14,7 +14,23 @@
 #include <stm32f4xx_hal_def.h>
 #include "GSE/sensor.h"
 #include <debug/led.h>
+#include <thread_init.h>
 
+#define VSENSE 				(3300.0/4095.0) //For conversions
+#define HOSE_PRESSURE		(1)
+#define HOSE_PRESSURE_CONV	(0.0649)
+#define HOSE_TEMP			(2)
+#define HOSE_TEMP_CONV		(1) //TODO
+#define TANK_TEMP			(3)
+#define TANK_TEMP_CONV		(1) //TODO
+#define WIND_SPEED			(4)
+#define WIND_SPEED_CONV		(1) //TODO
+#define BATTERY_LEVEL		(5)
+#define BATTERY_LEVEL_CONV	(490/100)
+#define ROCKET_WEIGHT		(6)
+#define ROCKET_WEIGHT_CONV	(1) //TODO
+#define CURRENT				(7)
+#define CURRENT_CONV		(1) //TODO
 
 float raw_tank_temp;
 float real_tank_temp;
@@ -69,10 +85,10 @@ void sensors_init(void)
 void TK_sensors_control(void const * argument)
 {
 	led_set_TK_rgb(led_GSE_sensor_id, 255, 0, 255);
+//	HAL_ADC_Start(&hadc1);
 
 	for(;;)
 	{
-
 		//read_tank_temp();
 		//read_anemo();
 		//read_hose_temp(); // does it work ?
@@ -80,9 +96,9 @@ void TK_sensors_control(void const * argument)
 		//read_load_cell();
 		//read_battery_level();
 		//read_current_level();
-
 		HAL_ADC_Start(&hadc1);
-#ifdef HB2_SENSOR_TELEMETRY_BOARD
+		HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+#ifdef HB1_SENSOR_TELEMETRY_BOARD
 		raw_hose_pressure = HAL_ADC_GetValue(&hadc1);
 		raw_hose_temp = HAL_ADC_GetValue(&hadc1);
 		raw_tank_temp = HAL_ADC_GetValue(&hadc1);
@@ -90,7 +106,7 @@ void TK_sensors_control(void const * argument)
 		raw_battery_level = HAL_ADC_GetValue(&hadc1);
 		raw_rocket_weight = HAL_ADC_GetValue(&hadc1);
 #endif
-#ifdef HB1_CODE_BOARD
+#ifdef HB2_CODE_BOARD
 		raw_current_ign_bckp = HAL_ADC_GetValue(&hadc1);
 		raw_current_dc_bckp = HAL_ADC_GetValue(&hadc1);
 #endif
@@ -100,159 +116,85 @@ void TK_sensors_control(void const * argument)
 		raw_current_fill = HAL_ADC_GetValue(&hadc1);
 		raw_current_purge = HAL_ADC_GetValue(&hadc1);
 #endif
-		HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
 	    HAL_ADC_Stop(&hadc1);
+
+#ifdef HB1_SENSOR_TELEMETRY_BOARD
+	    real_hose_pressure = sensor_conversion(HOSE_PRESSURE, raw_hose_pressure);
+		can_setFrame(real_hose_pressure, DATA_ID_IN_HOSE_PRESSURE, HAL_GetTick());
+
+	    real_hose_temp = sensor_conversion(HOSE_TEMP, raw_hose_temp);
+		can_setFrame(real_hose_temp, DATA_ID_IN_HOSE_TEMPERATURE, HAL_GetTick());
+
+	    real_tank_temp = sensor_conversion(TANK_TEMP, raw_tank_temp);
+		can_setFrame(real_tank_temp, DATA_ID_TANK_TEMPERATURE, HAL_GetTick());
+
+	    real_wind_speed = sensor_conversion(WIND_SPEED, raw_wind_speed);
+		can_setFrame(real_wind_speed, DATA_ID_WIND_SPEED, HAL_GetTick());
+
+	    real_battery_level = sensor_conversion(BATTERY_LEVEL, raw_battery_level);
+		can_setFrame(real_battery_level, DATA_ID_BATTERY_LEVEL, HAL_GetTick());
+
+	    real_rocket_weight = sensor_conversion(ROCKET_WEIGHT, raw_rocket_weight);
+		can_setFrame(real_rocket_weight, DATA_ID_ROCKET_WEIGHT, HAL_GetTick());
+
+#endif
+#ifdef HB2_CODE_BOARD
+	    real_current_ign_bckp = sensor_conversion(CURRENT, raw_current_ign_bckp);
+		can_setFrame(real_current_ign_bckp, DATA_ID_IGNITION_CURRENT_2, HAL_GetTick());
+
+	    real_current_dc_bckp = sensor_conversion(CURRENT, raw_current_dc_bckp);
+		can_setFrame(real_current_dc_bckp, DATA_ID_DISCONNECT_CURRENT_2, HAL_GetTick());
+
+#endif
+#ifdef HB3_POWER_BOARD
+		real_current_ign_main = sensor_conversion(CURRENT, raw_current_ign_main);
+		can_setFrame(real_current_ign_main, DATA_ID_IGNITION_CURRENT_1, HAL_GetTick());
+
+		real_current_dc_main = sensor_conversion(CURRENT, raw_current_dc_main);
+		can_setFrame(real_current_dc_main, DATA_ID_DISCONNECT_CURRENT_1, HAL_GetTick());
+
+		real_current_fill = sensor_conversion(CURRENT, raw_current_fill);
+		can_setFrame(real_current_fill, DATA_ID_FILL_VALVE_CURRENT, HAL_GetTick());
+
+		real_current_purge = sensor_conversion(CURRENT, raw_current_purge);
+		can_setFrame(real_current_purge, DATA_ID_PURGE_VALVE_CURRENT, HAL_GetTick());
+
+#endif
 
 		osDelay(100);
 	}
 }
 
-
-// ====================== OBSOLETE ======================
-void read_tank_temp(void)
+float sensor_conversion(uint8_t sensor, float raw_data)
 {
-	/*
-	 * 10mV/°C
-	 * 2°C = 0mV
-	 * 102°C = 1V
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_tank_temp = HAL_ADC_GetValue(&hadc1);
-
-	real_tank_temp = (float) raw_tank_temp*(3300.0/4095.0);
-	//result in mV or .1°C
-	//Conversion from mV to 0.1°C
-	//real_tank_temp = raw_tank_temp*(1.5*4095.0)/3.3;
-	// (VALUE (mV) - min (mV))/max (mV) * max (UNIT)
-
-	can_setFrame(real_tank_temp, DATA_ID_TANK_TEMPERATURE, HAL_GetTick());
-}
-void read_anemo(void)
-{
-	/*
-	 * Output: 0.4V - 2V
-	 * Min: 0.2 m/s
-	 * Max: 32.4 m/s
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_wind_speed = HAL_ADC_GetValue(&hadc1);
-
-	real_wind_speed = 330*((float) raw_wind_speed/4095.0); //result in 10mV
-	real_wind_speed = ((float) real_wind_speed - 40.0 )/ 160.0*32.4; //result in 0.1m/s
-	// (VALUE (mV) - min (mV))/max (mV) * max (UNIT)
-
-	can_setFrame(real_wind_speed, DATA_ID_WIND_SPEED, HAL_GetTick());
-}
-void read_hose_temp(void)
-{
-	/*
-	 * UNITS / RATE
-	 * Min - Max values
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_hose_temp = HAL_ADC_GetValue(&hadc1);
-
-	real_hose_temp = 3300*((float) raw_hose_temp/4095.0); //result in mV
-	//Conversion from mV to 0.1°C
-	// (VALUE (mV) - min (mV))/max (mV) * max (UNIT)
-
-	can_setFrame(real_hose_temp, DATA_ID_IN_HOSE_TEMPERATURE, HAL_GetTick());
-}
-void read_hose_pressure(void)
-{
-	/*
-	 * UNITS / RATE
-	 * Min - Max values
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_hose_pressure = HAL_ADC_GetValue(&hadc1);
-
-	real_hose_pressure = 3300*((float) raw_hose_pressure/4095.0); //result in mV
-	//Conversion from mV to 0.1bar
-	// (VALUE (mV) - min (mV))/max (mV) * max (UNIT)
-
-	can_setFrame(real_hose_pressure, DATA_ID_IN_HOSE_PRESSURE, HAL_GetTick());
-}
-void read_load_cell(void)
-{
-	/*
-	 * UNITS / RATE
-	 * Min - Max values
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_rocket_weight = HAL_ADC_GetValue(&hadc1);
-
-	real_rocket_weight = 3300*((float) raw_rocket_weight/4095.0); //result in mV
-	//Conversion from mV to 0.1kg
-	// (VALUE (mV) - min (mV))/max (mV) * max (UNIT)
-
-	can_setFrame(real_rocket_weight, DATA_ID_ROCKET_WEIGHT, HAL_GetTick());
+	float converted_value = 0;
+	converted_value = raw_data*VSENSE;
+	switch(sensor)
+	{
+	case HOSE_PRESSURE:
+		converted_value /= HOSE_PRESSURE_CONV;
+		break;
+	case HOSE_TEMP:
+		converted_value /= HOSE_TEMP_CONV;
+		break;
+	case TANK_TEMP:
+		converted_value /= TANK_TEMP_CONV;
+		break;
+	case WIND_SPEED:
+		converted_value /= WIND_SPEED_CONV;
+		break;
+	case BATTERY_LEVEL:
+		converted_value /= BATTERY_LEVEL_CONV;
+		break;
+	case ROCKET_WEIGHT:
+		converted_value /= ROCKET_WEIGHT_CONV;
+		break;
+	case CURRENT:
+		converted_value /= CURRENT_CONV;
+		break;
+	default:
+		break;
+	}
+	return converted_value;
 }
 
-void read_battery_level(void)
-{
-	/*
-	 * UNITS / RATE
-	 * Min - Max values
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-
-	raw_battery_level = HAL_ADC_GetValue(&hadc1);
-
-	real_battery_level = 3300*((float) raw_battery_level/4095.0); //result in mV
-
-	can_setFrame(real_battery_level, DATA_ID_BATTERY_LEVEL, HAL_GetTick());
-}
-
-void get_current_level(void)
-{
-	/*
-	 * UNITS / RATE
-	 * Min - Max values
-	 *
-	 */
-
-	// Get ADC value
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	raw_current = HAL_ADC_GetValue(&hadc1);
-
-	real_current = 3300*((float) raw_current/4095.0); //result in mV
-
-	//TODO Specify distribution method
-
-	//#ifdef HB1_CODE_BOARD
-	//		can_setFrame(current, DATA_ID_IGNITION_CURRENT_1, HAL_GetTick());
-	//		can_setFrame(current, DATA_ID_DISCONNECT_CURRENT_1, HAL_GetTick());
-	//#endif
-	//#ifdef HB3_POWER_BOARD
-	//		can_setFrame(current, DATA_ID_IGNITION_CURRENT_2, HAL_GetTick());
-	//		can_setFrame(current, DATA_ID_DISCONNECT_CURRENT_2, HAL_GetTick());
-	//		can_setFrame(current, DATA_ID_FILL_VALVE_CURRENT, HAL_GetTick());
-	//		can_setFrame(current, DATA_ID_PURGE_VALVE_CURRENT, HAL_GetTick());
-	//#endif
-
-}
