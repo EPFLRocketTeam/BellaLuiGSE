f9272aeadc3839cb275b5011b2ba69e44743104a
Lucas Pallez <lucas.pallez@hotmail.com>
2021-05-02 13:20:16 +0200

    Removed need for security code for the disconnect

--- Application/Src/GSE/order.c
+++ Application/Src/GSE/order.c
@@ -40,121 +40,103 @@ void order_sys_init(void)
 
 void TK_order_control(void const * argument)
 {
-
 	static uint32_t order = 0;
 
 	//TODO Add sensor confirmation
-	 for(;;) {
+	 for(;;)
+	 {
 		 order = can_getOrder();
-			 switch (order)
-			{
-				case OPEN_FILL_VALVE:
-				{
-					//rocket_log("Open Fill\n");
-					HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+		 switch (order)
+		 {
+		 	 case OPEN_FILL_VALVE:
+		 	 {
+				//rocket_log("Open Fill\n");
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
 
-					can_setFrame(GPIO_PIN_SET, DATA_ID_FILL_VALVE_STATE, HAL_GetTick());
-					break;
-				}
-				case CLOSE_FILL_VALVE:
-				{
-					//rocket_log("Close Fill\n");
-					HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-
-					can_setFrame(GPIO_PIN_RESET, DATA_ID_FILL_VALVE_STATE, HAL_GetTick());
-					break;
-				}
-				case OPEN_PURGE_VALVE:
-				{
-					//rocket_log("Open Purge\n");
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
-					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
-
-					can_setFrame(GPIO_PIN_SET, DATA_ID_PURGE_VALVE_STATE, HAL_GetTick());
-					break;
-				}
-				case CLOSE_PURGE_VALVE:
-				{
-					//rocket_log("Close Purge\n");
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
-					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
-
-					can_setFrame(GPIO_PIN_RESET, DATA_ID_PURGE_VALVE_STATE, HAL_GetTick());
-					break;
-				}
-				case MAIN_DISCONNECT_ON: //Main Disconnect On
-				{
-					if(verify_security_code())
-					{
-#ifdef HB3_POWER_BOARD
-						//rocket_log("MAIN DISCONNECT ON!\n");
-						HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); //Turn on S1_D3
-						HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn on S1_D0
-						can_setFrame(GPIO_PIN_SET, DATA_ID_MAIN_DISCONNECT_STATE, HAL_GetTick());
-#endif
-#ifdef HB2_CODE_BOARD
-						//rocket_log("SEC DISCONNECT ON!\n");
-						HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET); //Turn off S1_D2
-						HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn off S1_D0
-						can_setFrame(GPIO_PIN_SET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
-
-#endif
-					}
-					else
-					{
-						led_set_TK_rgb(led_GSE_order_id, 255, 0, 0);
-//						can_setFrame(0, DATA_ID_GST_CODE, HAL_GetTick());
-					}
-					break;
-				}
-				case MAIN_DISCONNECT_OFF: //Main Disconnect Off
-				{
-#ifdef HB3_POWER_BOARD
-					//rocket_log("MAIN DISCONNECT OFF!\n");
-					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); //Turn off S1_D3
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
-					can_setFrame(GPIO_PIN_RESET, DATA_ID_MAIN_DISCONNECT_STATE, HAL_GetTick());
-#endif
-#ifdef HB2_CODE_BOARD
-					//rocket_log("SEC DISCONNECT OFF!\n");
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET); //Turn off S1_D2
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
-					can_setFrame(GPIO_PIN_RESET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
-#endif
-					break;
-
-				}
-#ifdef HB2_CODE_BOARD
-				case SECONDARY_DISCONNECT_ON: //Secondary Disconnect On
-				{
-					if(verify_security_code())
-					{
-						//rocket_log("SEC DISCONNECT ON!\n");
-						HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET); //Turn on S1_D2
-						HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn on S1_D0
-						can_setFrame(GPIO_PIN_SET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
-					}
-					else
-					{
-						led_set_TK_rgb(led_GSE_order_id, 255, 0, 0);
-//						can_setFrame(0, DATA_ID_GST_CODE, HAL_GetTick());
-					}
-					break;
-				}
-				case SECONDARY_DISCONNECT_OFF: //Secondary Ignition Off
-				{
-					//rocket_log("SEC DISCONNECT OFF!\n");
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET); //Turn off S1_D2
-					HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
-					can_setFrame(GPIO_PIN_RESET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
-					break;
-				}
-#endif
+				can_setFrame(GPIO_PIN_SET, DATA_ID_FILL_VALVE_STATE, HAL_GetTick());
+				break;
 			}
+			case CLOSE_FILL_VALVE:
+			{
+				//rocket_log("Close Fill\n");
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+
+				can_setFrame(GPIO_PIN_RESET, DATA_ID_FILL_VALVE_STATE, HAL_GetTick());
+				break;
+			}
+			case OPEN_PURGE_VALVE:
+			{
+				//rocket_log("Open Purge\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
+
+				can_setFrame(GPIO_PIN_SET, DATA_ID_PURGE_VALVE_STATE, HAL_GetTick());
+				break;
+			}
+			case CLOSE_PURGE_VALVE:
+			{
+				//rocket_log("Close Purge\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+
+				can_setFrame(GPIO_PIN_RESET, DATA_ID_PURGE_VALVE_STATE, HAL_GetTick());
+				break;
+			}
+			case MAIN_DISCONNECT_ON: //Main Disconnect On
+			{
+#ifdef HB3_POWER_BOARD
+				//rocket_log("MAIN DISCONNECT ON!\n");
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); //Turn on S1_D3
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn on S1_D0
+				can_setFrame(GPIO_PIN_SET, DATA_ID_MAIN_DISCONNECT_STATE, HAL_GetTick());
+#endif
+#ifdef HB2_CODE_BOARD
+				//rocket_log("SEC DISCONNECT ON!\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET); //Turn off S1_D2
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn off S1_D0
+				can_setFrame(GPIO_PIN_SET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
+
+#endif
+				break;
+			}
+			case MAIN_DISCONNECT_OFF: //Main Disconnect Off
+			{
+#ifdef HB3_POWER_BOARD
+				//rocket_log("MAIN DISCONNECT OFF!\n");
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET); //Turn off S1_D3
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
+				can_setFrame(GPIO_PIN_RESET, DATA_ID_MAIN_DISCONNECT_STATE, HAL_GetTick());
+#endif
+#ifdef HB2_CODE_BOARD
+				//rocket_log("SEC DISCONNECT OFF!\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET); //Turn off S1_D2
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
+				can_setFrame(GPIO_PIN_RESET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
+#endif
+				break;
+			}
+#ifdef HB2_CODE_BOARD
+			case SECONDARY_DISCONNECT_ON: //Secondary Disconnect On
+			{
+				//rocket_log("SEC DISCONNECT ON!\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET); //Turn on S1_D2
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET); //Turn on S1_D0
+				can_setFrame(GPIO_PIN_SET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
+				break;
+			}
+			case SECONDARY_DISCONNECT_OFF: //Secondary Ignition Off
+			{
+				//rocket_log("SEC DISCONNECT OFF!\n");
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET); //Turn off S1_D2
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET); //Turn off S1_D0
+				can_setFrame(GPIO_PIN_RESET, DATA_ID_SEC_DISCONNECT_STATE, HAL_GetTick());
+				break;
+			}
+#endif
+		 }
 		 HAL_IWDG_Refresh(&hiwdg);
 		 osDelay(20);
 	 }
 }
-
